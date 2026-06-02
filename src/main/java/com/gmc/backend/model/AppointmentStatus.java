720b9afd91e48868ea43a7cf0e11384d856b7761
Thushakaran <thushakarant@gmail.com>
2026-06-02 08:39:41 +0530

    Revert "feat: add models"
    This reverts commit d0f0fd711cff563ab675563d43ef1eec79b34eeb.

--- src/main/java/com/gmc/backend/model/AppointmentStatus.java
+++
@@ -1,8 +0,0 @@
-package com.gmc.backend.model;
-
-public enum AppointmentStatus {
-    PENDING,
-    CONFIRMED,
-    CANCELLED,
-    COMPLETED
-}
--- src/main/java/com/gmc/backend/model/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.gmc.backend.model;
-
-public enum OrderStatus {
-    PENDING,
-    PROCESSING,
-    SHIPPED,
-    DELIVERED,
-    CANCELLED
-}
