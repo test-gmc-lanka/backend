0a63b1949b698cfe60505ce6fb4cefbe1cffb845
Tharshana11 <tharsh.sarath@gmail.com>
2026-06-02 17:52:44 +0530

    feat(model): add Cart entity

---
+++ src/main/java/com/gmc/backend/model/Cart.java
@@ -0,0 +1,22 @@
+package com.gmc.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems;
+}
