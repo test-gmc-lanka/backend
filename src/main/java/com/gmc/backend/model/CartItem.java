7ae7aca5a24a01b4bc25dfbc01d9fa0ce003c0b2
Tharshana11 <tharsh.sarath@gmail.com>
2026-06-02 17:53:00 +0530

    feat(model): add CartItem entity

---
+++ src/main/java/com/gmc/backend/model/CartItem.java
@@ -0,0 +1,25 @@
+package com.gmc.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "cart_items")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
