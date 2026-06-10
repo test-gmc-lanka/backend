7ffea72d0339e565c487e4b8ca3733ed9984ff20
Thushakaran <thushakarant@gmail.com>
2026-06-10 09:18:47 +0530

    refactor(repository): remove unused import statements from CustomDesignRepository

--- src/main/java/com/gmc/backend/repository/CustomDesignRepository.java
+++ src/main/java/com/gmc/backend/repository/CustomDesignRepository.java
@@ -2,8 +2,5 @@ package com.gmc.backend.repository;
 
 import com.gmc.backend.model.CustomDesign;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
 public interface CustomDesignRepository extends JpaRepository<CustomDesign, Long> {
 }
