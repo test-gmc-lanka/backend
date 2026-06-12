89bb989490cb845c389d4b25eeb26e0e0f267e11
Thushakaran <thushakarant@gmail.com>
2026-06-12 08:52:17 +0530

    Replace tabs with spaces in repositories

--- src/main/java/com/gmc/backend/repository/MaintenanceRepository.java
+++ src/main/java/com/gmc/backend/repository/MaintenanceRepository.java
@@ -1,7 +1,23 @@
-package com.gmc.backend.repository;
+﻿package com.gmc.backend.repository;
 
 import com.gmc.backend.model.Maintenance;
+import com.gmc.backend.model.Product;
+import com.gmc.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface MaintenanceRepository extends JpaRepository<Maintenance, Long> {
+
+    List<Maintenance> findByUser(User user);
+
+    List<Maintenance> findByUser_UserId(Long userId);
+
+    List<Maintenance> findByProduct(Product product);
+
+    List<Maintenance> findByStatus(String status);
+
 }
+
