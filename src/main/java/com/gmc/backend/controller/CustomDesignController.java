a6b914ad47b376cd015baf5992aeed42f5937393
Thushakaran <thushakarant@gmail.com>
2026-07-03 10:54:54 +0530

    feat(custom-design): enhance CustomDesignController and ProductController with additional mappings and imports

--- src/main/java/com/gmc/backend/controller/CustomDesignController.java
+++ src/main/java/com/gmc/backend/controller/CustomDesignController.java
@@ -8,7 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
